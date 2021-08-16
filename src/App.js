4b9e01f0296af5ccc81272eb309234f915dd9669
shivaraj shooru <shivarajshooru@gmail.com>
2021-08-16 12:30:58 +0530

    done 1230

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: 0, x: 0, y: 0, started: false };
+    this.timerInt = null;
+    this.gameStart = this.gameStart.bind(this);
+    this.keyListener = this.keyListener.bind(this);
+  }
+
+  gameStart() {
+    this.setState({
+      started: true
+    });
+    this.timerInt = setInterval(
+      () => this.setState({ time: this.state.time + 1 }),
+      1 * 1000
+    );
+    document.addEventListener("keydown", this.keyListener);
+  }
+
+  keyListener(evt) {
+    if (this.state.started) {
+      if (evt.keyCode === 37) {
+        this.setState({ x: this.state.x - 5 });
+      } else if (evt.keyCode === 38) {
+        this.setState({ y: this.state.y - 5 });
+      } else if (evt.keyCode === 39) {
+        this.setState({ x: this.state.x + 5 });
+      } else if (evt.keyCode === 40) {
+        this.setState({ y: this.state.y + 5 });
+      }
+    }
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.timerInt);
+      document.removeEventListener("keydown", this.keyListener);
+      const p = document.createElement("p");
+      p.innerHTML = "you won the game";
+      document.body.appendChild(p);
+    }
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <>
+        {!this.state.started ? (
+          <button className="start" onClick={this.gameStart}>
+            start
+          </button>
+        ) : (
+          <>
+            <div
+              className="ball"
+              style={{ left: this.state.x + "px", top: this.state.y + "px" }}
+            ></div>
+          </>
+        )}
+        <div className="hole" style={{ left: "250px", top: "250px" }}></div>
+        <div className="heading-timer">{this.state.time}</div>
+      </>
+    );
+  }
 }
 
-export default App;
+export default Timer;
